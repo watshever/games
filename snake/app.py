a21e508fa4931afa42c275d7964202a0caa5c406
watshever <louieweng@outlook.com>
2020-05-16 19:28:27 -0400

    abstracted the game away from the main app component

--- snake/app.py
+++ snake/app.py
@@ -1,4 +1,4 @@
-from objects.game import GAME_WIDTH, GAME_HEIGHT
+from objects.game import Game
 from objects.food import Food
 from objects.snake import Snake
 
@@ -6,30 +6,18 @@ import pygame
 import os
 os.environ['SDL_AUDIODRIVER']='dsp'
 
-# initialize pygame components 
+# initialize pygame components and game
 pygame.init()
-screen = pygame.display.set_mode((GAME_HEIGHT, GAME_WIDTH))
-clock = pygame.time.Clock()
-
-# create food and snake
-food = Food((34, 231, 2))
-snake = Snake((255, 64, 89))
+game = Game()
 
 # condition to run the game
 done = False
 
 # main game loop
 while not done:
-	
-	# render the animation of the game
-	clock.tick(100 * (1 + len(snake.body) / 50))
-	screen.fill(pygame.Color(0,0,0))
-	food.render(screen)
-	snake.render(screen)
 
-	# update the snake location and check lose conditions
-	snake.snake_controls(food)
-	done = snake.check_lose()
+	# run the snake game
+	done = game.run()
 
 	# handler for exit button on gui
 	for event in pygame.event.get():
--- snake/objects/game.py
+++ snake/objects/game.py
@@ -1,8 +1,41 @@
+from objects.food import Food
+from objects.snake import Snake
+import pygame
+
 # global game variables for access by all modules
 
+# gameboard controls
 GAME_WIDTH = 400
 GAME_HEIGHT = 400
+CLOCK_SPEED = 100
+
+# snake controls
 SNAKE_BODY_SIZE = 10
+SNAKE_COLOR = (255, 64, 89)
+
+# food controls
 FOOD_SIZE = 10
+FOOD_COLOR = (34, 231, 2)
 
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode((GAME_HEIGHT, GAME_WIDTH))
+        self.clock = pygame.time.Clock()
+        self.food = Food(FOOD_COLOR)
+        self.snake = Snake(SNAKE_COLOR)
+        self.game_over = False
 
+    def run(self):
+        # update core game components
+        self.clock.tick(100 * (1 + len(self.snake.body) / 50))
+        self.screen.fill(pygame.Color(0,0,0))
+	    
+        # render the snake and food for the game
+        self.food.render(self.screen)
+        self.snake.render(self.screen)
+
+        # update the snake and check for lose conditions
+        self.snake.snake_controls(self.food)
+        self.game_over = self.snake.check_lose()
+
+        return self.game_over
